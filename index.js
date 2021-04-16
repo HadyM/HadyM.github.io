fa9cd06e05b3d406f15c1f0dce61ef38a5e17bdd
Hady Mohamed <hadymohamed@pursuit.org>
2021-04-15 21:07:12 -0400

    minor fixes

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 const splash = document.querySelector(".splash");
 
-document.addEventListener("DOMContentLoaded", (e) => {
-  setTimeout(() => {
-    splash.classList.add("display-none");
-  }, 4000);
+setTimeout(() => {
+  splash.classList.add("display-none");
+}, 4000);
 
-  const navToggle = document.querySelector(".nav-toggle");
-  const navLinks = document.querySelectorAll(".nav__link");
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
 
-  navToggle.addEventListener("click", () => {
-    document.body.classList.toggle("nav-open");
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
   });
+});
 
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      document.body.classList.remove("nav-open");
-    });
-  });
+// const navTools = document.querySelectorAll(".nav__links, .nav-toggle");
+// navTools.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     document.body.classList.toggle("nav-open");
+//     // document.body.classList.remove("nav-open");
+//   });
+// });
 
-  // Animation
-  let typed = new Typed(".typing", {
-    strings: [
-      "Full-Stack Developer",
-      "Graphic Designer",
-      "Gamer",
-      "Anime lover",
-      "Chef",
-    ],
-    typeSpeed: 100,
-    backSpeed: 60,
-    smartBackspace: true,
-    loop: true,
-  });
+// Animation
+let typed = new Typed(".typing", {
+  strings: [
+    "Full-Stack Developer",
+    "Graphic Designer",
+    "Gamer",
+    "Anime lover",
+    "Chef",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  smartBackspace: true,
+  loop: true,
 });
