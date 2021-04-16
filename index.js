31c95e222d61bd7b79180849fa66497aef948188
Hady Mohamed <hadymohamed@pursuit.org>
2021-04-15 20:04:38 -0400

    fixed all the comments Myra made

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-const splash = document.querySelector('.splash')
+const splash = document.querySelector(".splash");
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    setTimeout(() => {
-        splash.classList.add('display-none')
-    }, 4000)
-})
+document.addEventListener("DOMContentLoaded", (e) => {
+  setTimeout(() => {
+    splash.classList.add("display-none");
+  }, 4000);
 
+  const navToggle = document.querySelector(".nav-toggle");
+  const navLinks = document.querySelectorAll(".nav__link");
 
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+  navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
+  });
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open')
-})
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      document.body.classList.remove("nav-open");
+    });
+  });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open')
-    })
-})
-
-
-// Animation
-let typed = new Typed(".typing", {
-    strings: ["Full-Stack Developer", "Graphic Designer", "Gamer", "Anime lover", "Chef"],
+  // Animation
+  let typed = new Typed(".typing", {
+    strings: [
+      "Full-Stack Developer",
+      "Graphic Designer",
+      "Gamer",
+      "Anime lover",
+      "Chef",
+    ],
     typeSpeed: 100,
     backSpeed: 60,
     smartBackspace: true,
-    loop: true
-})
-
-
-
-
-
-
-
+    loop: true,
+  });
+});
