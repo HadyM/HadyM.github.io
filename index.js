f6704a4532132c5fc589674cb00d3dc7c8a1869f
Hady Mohamed <hadymohamed@pursuit.org>
2021-04-19 20:04:32 -0400

    Fixed all the issues that Myra suggested evrything works as it should now

--- index.js
+++ index.js
@@ -4,26 +4,12 @@ setTimeout(() => {
   splash.classList.add("display-none");
 }, 4000);
 
-const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll(".nav__link");
-
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
-});
-
-navLinks.forEach((link) => {
+const navTools = document.querySelectorAll(".nav__link, .nav-toggle");
+navTools.forEach((link) => {
   link.addEventListener("click", () => {
-    document.body.classList.remove("nav-open");
+    document.body.classList.toggle("nav-open");
   });
 });
-
-// const navTools = document.querySelectorAll(".nav__links, .nav-toggle");
-// navTools.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     document.body.classList.toggle("nav-open");
-//     // document.body.classList.remove("nav-open");
-//   });
-// });
 
 // Animation
 let typed = new Typed(".typing", {
